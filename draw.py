c22621b90b22b4b38aeecae1c898ed42a86ea52b
Luka Stopar <luka.stopar@ijs.si>
2016-07-22 19:57:20 +0200

    fix

--- draw.py
+++ draw.py
@@ -10,30 +10,37 @@ def _draw(start, end, rounds, player_ids):
         n_rounds = max(l1, l2)
         
         # divide
-        _draw(start, middle, rounds, player_ids)
-        _draw(middle+1, end, rounds, player_ids)
+        offset1 = _draw(start, middle, rounds, player_ids)
+        offset2 = _draw(middle+1, end, rounds, player_ids)
         
         # conquer
-        round_offset = (1 << (int(ceil(log(n, 2))) - 1)) - 1
+        round_offset = max(offset1, offset2) + 1# = (1 << (int(ceil(log(n, 2))) - 1)) - 1
+        max_round = 0;
         for i in range(l1):
             for j in range(l2):
                 diff = i - j
                 local_round = diff % n_rounds
-                rounds[local_round + round_offset].append((player_ids[start + i], player_ids[middle + j + 1]))
-
+                _round = local_round + round_offset
+                rounds[_round].append((player_ids[start + i], player_ids[middle + j + 1]))
+                if _round > max_round:
+                    max_round = _round
+                    
+        return max_round
+    else:
+        return -1
 
 def draw(player_ids):
     n_players = len(player_ids)
     max_depth = int(ceil(log(n_players, 2)))
     
-    total_rounds = (1 << (max_depth)) - 1
-    rounds = [[] for _ in range(total_rounds)]
+    possible_rounds = (1 << (max_depth)) - 1
+    rounds = [[] for _ in range(possible_rounds)]
     
     _draw(0, n_players - 1, rounds, player_ids)
     
     return rounds
     
-n = 17
+n = 64
 rounds = draw(range(n))
 
 matches = n*(n-1) / 2
