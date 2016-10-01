1a90c7094ad6fca2bc541d1d899d17397f5667a8
Luka Stopar <luka.stopar@ijs.si>
2016-10-01 14:59:24 +0200

    first version with first test

---
+++ test/test.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+
+if len(sys.argv) <= 1:
+    print 'Usage: python test.py $FILE_NAME'
+    exit(1)
+
+url = 'http://localhost:' + sys.argv[2] + '/' if len(sys.argv) == 3 else 'http://localhost:8987/'
+fname = sys.argv[1]
+
+def read_file(fname):
+    f = open(fname, 'r')
+    result = f.read()
+    f.close()
+    return result
+
+if __name__ == '__main__':
+    data = read_file(fname)
+    
+    print 'data:'
+    print data
+
+    req = requests.post(url, data=data)
+
+    print 'Response:'
+    print req.text
